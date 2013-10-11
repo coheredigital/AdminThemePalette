1bf320f702e7d6cc1b61909f05ef99e09d8e57fe
Adam Spruijt <adamspruijt@gmail.com>
2013-10-11 19:16:57 -0400

    sooooo awesome

--- templates-admin/default.php
+++ templates-admin/default.php
@@ -6,48 +6,48 @@ $bodyClass .= "id-{$page->id} template-{$page->template->name}";
 if(!isset($content)) $content = '';
 
 $defaultColorTheme = 'default';
-if($input->get->colors && !$user->isGuest()) $colors = $sanitizer->pageName($input->get->colors); 
-	else if($session->adminThemeColors) $colors = $session->adminThemeColors; 
-	else if($config->adminThemeColors) $colors = $sanitizer->pageName($config->adminThemeColors); 
+if($input->get->colors && !$user->isGuest()) $colors = $sanitizer->pageName($input->get->colors);
+	else if($session->adminThemeColors) $colors = $session->adminThemeColors;
+	else if($config->adminThemeColors) $colors = $sanitizer->pageName($config->adminThemeColors);
 	else $colors = $defaultColorTheme;
 
 if(is_file(dirname(__FILE__) . "/styles/main-$colors.css")) $session->adminThemeColors = $colors;
 	else $session->adminThemeColors = $defaultColorTheme;
 
-$config->styles->prepend($config->urls->adminTemplates . "styles/main-$session->adminThemeColors.css?v=6"); 
-$config->styles->append($config->urls->root . "wire/templates-admin/styles/font-awesome/css/font-awesome.min.css"); 
-$config->scripts->append($config->urls->root . "wire/templates-admin/scripts/inputfields.js?v=5"); 
-$config->scripts->append($config->urls->adminTemplates . "scripts/main.js?v=5"); 
+$config->styles->prepend($config->urls->adminTemplates . "styles/main-$session->adminThemeColors.css?v=6");
+$config->styles->append($config->urls->root . "wire/templates-admin/styles/font-awesome/css/font-awesome.min.css");
+$config->scripts->append($config->urls->root . "wire/templates-admin/scripts/inputfields.js?v=5");
+$config->scripts->append($config->urls->adminTemplates . "scripts/main.js?v=5");
 
-$browserTitle = wire('processBrowserTitle'); 
+$browserTitle = wire('processBrowserTitle');
 if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FILE__) . ' &bull; ProcessWire';
 
 /*
  * Dynamic phrases that we want to be automatically translated
  *
- * These are in a comment so that they register with the parser, in place of the dynamic __() function calls with page titles. 
- * 
- * __("Pages"); 
- * __("Setup"); 
- * __("Modules"); 
- * __("Access"); 
- * __("Admin"); 
- * __("Site"); 
- * __("Languages"); 
- * __("Users"); 
- * __("Roles"); 
- * __("Permissions"); 
- * __("Templates"); 
- * __("Fields"); 
- * __("Add New"); 
- * __("Not yet configured: See template family settings."); 
- * 
+ * These are in a comment so that they register with the parser, in place of the dynamic __() function calls with page titles.
+ *
+ * __("Pages");
+ * __("Setup");
+ * __("Modules");
+ * __("Access");
+ * __("Admin");
+ * __("Site");
+ * __("Languages");
+ * __("Users");
+ * __("Roles");
+ * __("Permissions");
+ * __("Templates");
+ * __("Fields");
+ * __("Add New");
+ * __("Not yet configured: See template family settings.");
+ *
  */
 
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo __('en', __FILE__); // HTML tag lang attribute
-	/* this intentionally on a separate line */ ?>"> 
+	/* this intentionally on a separate line */ ?>">
 <head>
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="robots" content="noindex, nofollow" />
@@ -62,16 +62,16 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 		$jsConfig = $config->js();
 		$jsConfig['debug'] = $config->debug;
 		$jsConfig['urls'] = array(
-			'root' => $config->urls->root, 
-			'admin' => $config->urls->admin, 
-			'modules' => $config->urls->modules, 
-			'core' => $config->urls->core, 
-			'files' => $config->urls->files, 
+			'root' => $config->urls->root,
+			'admin' => $config->urls->admin,
+			'modules' => $config->urls->modules,
+			'core' => $config->urls->core,
+			'files' => $config->urls->files,
 			'templates' => $config->urls->templates,
 			'adminTemplates' => $config->urls->adminTemplates,
-			); 
+			);
 
-		if(!empty($jsConfig['JqueryWireTabs'])) $bodyClass .= ' hasWireTabs'; 
+		if(!empty($jsConfig['JqueryWireTabs'])) $bodyClass .= ' hasWireTabs';
 		?>
 
 		var config = <?php echo json_encode($jsConfig); ?>;
@@ -82,7 +82,7 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 
 	<!--[if IE]>
 	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->adminTemplates; ?>styles/ie.css" />
-	<![endif]-->	
+	<![endif]-->
 
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->adminTemplates; ?>styles/ie7.css" />
@@ -97,31 +97,14 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 
 	<?php if(!$user->isGuest()): ?>
 
-	<div id='breadcrumbs'>
-		<div class='container'>
 
-			
 
-			<ul class='nav'>
-
-				<?php
-				echo "<li><a class='sitelink' href='{$config->urls->root}'><i class='icon-home'></i></a><i class='icon-angle-right'></i></li>"; 
-				foreach($this->fuel('breadcrumbs') as $breadcrumb) {
-					$title = __($breadcrumb->title, __FILE__); 
-					echo "<li><a href='{$breadcrumb->url}'>{$title}</a><i class='icon-angle-right'></i></li>";
-				}
-				unset($title);
-				?>
-			</ul>
-		</div>
-	</div>
-
-	<?php endif; ?>	
+	<?php endif; ?>
 
 	<div id="masthead" class="masthead ui-helper-clearfix">
 		<div class="container">
 
-			
+
 			<?php echo tabIndent($searchForm, 3); ?>
 
 			<ul id='topnav'>
@@ -154,12 +137,24 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 		<div id="wrapper">
 			<div id="headline">
 				<div class="container">
-					<h1 id='title'><?php echo __(strip_tags($this->fuel->processHeadline ? $this->fuel->processHeadline : $page->get("title|name")), __FILE__); ?></h1>
+					<div id='breadcrumbs'>
+						<ul class='nav'>
+							<?php
+							echo "<li><a class='sitelink' href='{$config->urls->root}'><i class='icon-home'></i></a><i class='icon-angle-right'></i></li>";
+							foreach($this->fuel('breadcrumbs') as $breadcrumb) {
+								$title = __($breadcrumb->title, __FILE__);
+								echo "<li><a href='{$breadcrumb->url}'>{$title}</a><i class='icon-angle-right'></i></li>";
+							}
+							unset($title);
+							echo "<li class='title'>" . __(strip_tags($this->fuel->processHeadline ? $this->fuel->processHeadline : $page->get("title|name")), __FILE__) . "</li>";
+							?>
+						</ul>
+					</div>
 
-					<?php 
+					<?php
 					if(in_array($page->id, array(2,3,8))) { // page-list
 						echo "<div id='head_button'>";
-						include($config->paths->adminTemplates . "shortcuts.inc"); 
+						include($config->paths->adminTemplates . "shortcuts.inc");
 						echo "</div>";
 					}
 					?>
@@ -174,7 +169,7 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 					<?php if($page->body) echo $page->body; ?>
 
 					<?php echo $content?>
-			</div>	
+			</div>
 		</div>
 
 	</div>
@@ -183,15 +178,15 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 
 	<div id="footer" class="footer">
 		<div class="container">
-			
+
 			<?php if(!$user->isGuest()): ?>
 
-			
+
 
 			<span id='userinfo'>
-				<i class='icon-user'></i> 
-				<?php 
-				if($user->hasPermission('profile-edit')): ?> 
+				<i class='icon-user'></i>
+				<?php
+				if($user->hasPermission('profile-edit')): ?>
 				<a class='action' href='<?php echo $config->urls->admin; ?>profile/'><?php echo $user->name; ?></a> |
 				<?php endif; ?>
 
@@ -200,10 +195,10 @@ if(!$browserTitle) $browserTitle = __(strip_tags($page->get('title|name')), __FI
 			<!-- <a id='logo' href='<?php echo $config->urls->admin?>'><img width='130' src="<?php echo $config->urls->adminTemplates?>styles/images/logo.png" alt="ProcessWire" /></a> -->
 			<?php endif; ?>
 			<p>
-				ProcessWire <?php echo $config->version . ' <!--v' . $config->systemVersion; ?>--> &copy; <?php echo date("Y"); ?> Ryan Cramer 
+				ProcessWire <?php echo $config->version . ' <!--v' . $config->systemVersion; ?>--> &copy; <?php echo date("Y"); ?> Ryan Cramer
 			</p>
-			
-			
+
+
 
 			<?php if($config->debug && $this->user->isSuperuser()) include($config->paths->adminTemplates . "debug.inc"); ?>
 		</div>
